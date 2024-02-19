425dc7097ce0a60f58fe31ebdbddcbf3fb4fb568
Gareth Dean <gazdean@hotmail.com>
2024-02-19 13:26:47 +0000

    current location utils function

---
+++ FlushFinder/src/utils.js
@@ -0,0 +1,23 @@
+import * as Location from "expo-location";
+
+export default function getCurrentLocation(){
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location.coords);
+
+      setInitialRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      });
+    };
+
+    getLocation();
+  }
