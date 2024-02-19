2b9735bb35762cd5245cdb500b1744366e5d4854
Gareth Dean <gazdean@hotmail.com>
2024-02-19 13:27:47 +0000

    home screen current location button

--- FlushFinder/src/screens/HomeScreen.js
+++ FlushFinder/src/screens/HomeScreen.js
@@ -1,3 +1,12 @@
-export default function HomeScreen () {
-  
+import { View, Button } from "react-native";
+
+import getCurrentLocation from "../utils";
+
+export default function HomeScreen() {
+    
+  return (
+    <View>
+      <Button title="Use Location" onPress={getCurrentLocation} />
+    </View>
+  );
 }
