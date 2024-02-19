d5661b90c44ec2f5f82ac408e8d5a7b984eb2519
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-19 16:59:18 +0000

    commented out dropdownpicker component that is causing issue, added placeholder for layout. Added filterform in the homescreen layout, no functionality has been made yet

--- FlushFinder/src/components/GeoLocationButton.js
+++ FlushFinder/src/components/GeoLocationButton.js
@@ -1,7 +1,8 @@
-import { View, Button } from "react-native";
+import { View } from "react-native";
 import { OriginLocationContext } from "../context/OriginLocation"; 
 import { useContext, useState } from "react";
 import { useNavigation } from '@react-navigation/native';
+import { Block, Button, Icon, Text, theme } from "galio-framework";
 
 import getCurrentLocation from "../utils";
 
@@ -17,9 +18,7 @@ export default function GeoLocationButton() {
     }
 
   return (
-    <View>
-      <Button title="Use Location" onPress={handleLocationButtonPress}/>
-    </View>
+      <Button onlyIcon icon="direction" iconFamily="Entypo" iconSize={25} iconColor="#fff" style={{ width: 40, height: 40 }} onPress={handleLocationButtonPress}>Use your current location</Button>
     
   );
 
