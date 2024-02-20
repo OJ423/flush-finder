8af9bddbd313687ab1d97a9b29938ceb67fc4146
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-20 15:11:40 +0000

    added splash screen component on the homepage screen

--- FlushFinder/src/screens/HomeScreen.js
+++ FlushFinder/src/screens/HomeScreen.js
@@ -1,32 +1,55 @@
-
 import DropDownPicker from "react-native-dropdown-picker";
 import GeoLocationButton from "../components/GeoLocationButton";
-import React from "react";
+import React, { useCallback, useEffect, useContext, useState } from "react";
 import {
   ImageBackground,
   StyleSheet,
   StatusBar,
   Dimensions,
 } from "react-native";
-import { Block, Button, Icon, Text, theme,Switch } from "galio-framework";
+import { Block, Button, Icon, Text, theme, Switch } from "galio-framework";
 import FilterForm from "../components/FilterForm";
-import { useContext, useState } from "react";
-
-
-
+import * as SplashScreen from "expo-splash-screen";
 
 const { height, width } = Dimensions.get("screen");
 const backGroungImg = require("../../assets/HomeBackground.jpg");
 
+SplashScreen.preventAutoHideAsync();
+
 export default function HomeScreen() {
   const [isUnisexOnly, setIsUnisexOnly] = useState(false);
-  const [isAccessibleOnly, setIsAccessibleOnly] = useState(true);
+  const [isAccessibleOnly, setIsAccessibleOnly] = useState(false);
   const [hasChangingTable, setHasChangingTable] = useState(false);
   const [hasError, setHasError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Makes the city api call here, put 'await '
+        // just a promise to check if the splashscreen work cab renove when we have api calls 
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      } catch (e) {
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isLoading === false) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
-    <Block flex style={styles.container}>
+    <Block flex style={styles.container} onLayout={onLayoutRootView}>
       <StatusBar barStyle="light-content" />
       <Block flex center>
         <ImageBackground
@@ -37,7 +60,7 @@ export default function HomeScreen() {
             marginTop: "-55%",
             zIndex: 1,
           }}
-          />
+        />
       </Block>
       <Block flex space="between" style={styles.padded}>
         <Block flex space="around" style={{ zIndex: 2 }}>
@@ -56,12 +79,25 @@ export default function HomeScreen() {
               <Button>Placeholder for dropdown picker</Button>
             </Block>
             <Block flex={1}>
-             <GeoLocationButton/>
+              <GeoLocationButton />
             </Block>
           </Block>
-          <FilterForm isAccessibleOnly={isAccessibleOnly} setIsAccessibleOnly={setIsAccessibleOnly} hasChangingTable={hasChangingTable} setHasChangingTable={setHasChangingTable} isUnisexOnly={isUnisexOnly} setIsUnisexOnly={setIsUnisexOnly}/>
+          <FilterForm
+            isAccessibleOnly={isAccessibleOnly}
+            setIsAccessibleOnly={setIsAccessibleOnly}
+            hasChangingTable={hasChangingTable}
+            setHasChangingTable={setHasChangingTable}
+            isUnisexOnly={isUnisexOnly}
+            setIsUnisexOnly={setIsUnisexOnly}
+          />
           <Block center>
-            <Button shadowless style={styles.button} onPress={() => {console.log(isAccessibleOnly,isUnisexOnly,hasChangingTable)}}>
+            <Button
+              shadowless
+              style={styles.button}
+              onPress={() => {
+                console.log(isAccessibleOnly, isUnisexOnly, hasChangingTable);
+              }}
+            >
               SUBMIT
             </Button>
           </Block>
@@ -70,7 +106,6 @@ export default function HomeScreen() {
     </Block>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
