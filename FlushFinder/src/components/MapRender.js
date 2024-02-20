5804ae79bbd74b19835456684757d596d5a1c25a
Gareth Dean <gazdean@hotmail.com>
2024-02-20 12:31:07 +0000

    changed toilet pin colour

--- FlushFinder/src/components/MapRender.js
+++ FlushFinder/src/components/MapRender.js
@@ -2,67 +2,72 @@ import { useState, useContext, useEffect } from "react";
 import { View, StyleSheet, ActivityIndicator } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-import { OriginLocationContext } from "../context/OriginLocation"; 
+import { OriginLocationContext } from "../context/OriginLocation";
 import { ToiletResponseContext } from "../context/ToiletResponse";
 
+
 export default function MapRender() {
-  const [initialRegion, setInitialRegion] = useState({})
+  const [initialRegion, setInitialRegion] = useState({});
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
-  const { originLocation } = useContext(OriginLocationContext)
-  const { toiletResponse } = useContext(ToiletResponseContext)
-
+  const { originLocation } = useContext(OriginLocationContext);
+  const { toiletResponse } = useContext(ToiletResponseContext);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     if (originLocation) {
       const newInitialRegion = {
-        latitude: originLocation.latitude + 0.01,
+        latitude: originLocation.latitude + 0.0075,
         longitude: originLocation.longitude,
         latitudeDelta: 0.03,
         longitudeDelta: 0.03,
       };
       setInitialRegion(newInitialRegion);
-      setIsLoading(false)
+      setIsLoading(false);
     }
   }, [originLocation]);
 
-return (
- isLoading?<View><ActivityIndicator size="large" color="blue"/></View>  : (
-  
-  <View style={styles.container}>
-    {initialRegion && (
-      <MapView style={styles.map} initialRegion={initialRegion}>
-        {originLocation && (
-          <Marker
-            coordinate={{
-              latitude: originLocation.latitude,
-              longitude: originLocation.longitude,
-            }}
-            title="Your Location"
-          ></Marker>
-        )}
-        {!toiletResponse ? null : toiletResponse.map((toilet) => (
-          <Marker
-           
-            key={toilet.id}
-            draggable
-            coordinate={{
-              latitude: toilet.latitude,
-              longitude: toilet.longitude,
-            }}
-            onDragEnd={(event) =>
-              alert(JSON.stringify(event.nativeEvent.coordinate))
-            }
-            title={toilet.name}
-            description={toilet.directions}
-          ></Marker>
-        ))}
-      </MapView>
-    )}
-  </View>)
-)}
+  return isLoading ? (
+    <View>
+      <ActivityIndicator size="large" color="blue" />
+    </View>
+  ) : (
+    <View style={styles.container}>
+      {initialRegion && (
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          {originLocation && (
+            <Marker
+              coordinate={{
+                latitude: originLocation.latitude,
+                longitude: originLocation.longitude,
+              }}
+              title="Your Location"
+            ></Marker>
+          )}
+          {!toiletResponse
+            ? null
+            : toiletResponse.map((toilet) => (
+                <Marker
+                pinColor={'blue'}
+                  key={toilet.id}
+                  draggable
+                  coordinate={{
+                    latitude: toilet.latitude,
+                    longitude: toilet.longitude,
+                  }}
+                  onDragEnd={(event) =>
+                    alert(JSON.stringify(event.nativeEvent.coordinate))
+                  }
+                  title={toilet.name}
+                  description={toilet.directions}
+                ></Marker>
+              ))}
+        </MapView>
+      )}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -73,7 +78,7 @@ const styles = StyleSheet.create({
     paddingTop: 60,
   },
   map: {
-    width: 400,
-    height: 1000,
-  }
+    width: 350,
+    height: 400,
+  },
 });
