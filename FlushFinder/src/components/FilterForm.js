263b885ca45897240168a9ca4ae77a133caf1aad
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-20 11:25:09 +0000

    refactored code to use function instead of class component in homepage

--- FlushFinder/src/components/FilterForm.js
+++ FlushFinder/src/components/FilterForm.js
@@ -1,18 +1,23 @@
 import { Block, Text,Switch } from "galio-framework";
+import { useState } from "react";
 
 export default function FilterForm () {
+
+
+
   return (
     <Block row>
     <Block center flex={1}>
-  {/* <Switch color="white"></Switch> */}
+  {/* <Switch color="white"   value={this.state["switch-1"]}
+  onValueChange={() => this.toggleSwitch("switch-1")}></Switch> */}
   <Text color="white">Accessible?</Text>
     </Block>
     <Block  center flex={1}>
-  {/* <Switch color="info" trackColor="white"></Switch> */}
+  {/* <Switch color="white"></Switch> */}
   <Text color="white">Unisex?</Text>
     </Block>
     <Block center flex={1}>
-  {/* <Switch color="success" trackColor="white"></Switch> */}
+  {/* <Switch color="white"></Switch> */}
   <Text color="white">Changing table?</Text>
     </Block>
 </Block>
