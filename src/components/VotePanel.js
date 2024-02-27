64b209e460d05e2db6e5ed0f95c505205825d8d9
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-27 13:12:26 +0000

    fixed the ios bug where the vote panel is not visible on ios only

--- src/components/VotePanel.js
+++ src/components/VotePanel.js
@@ -2,40 +2,43 @@ import { useNavigation } from "@react-navigation/core";
 import { Block, Icon, Text } from "galio-framework";
 import { useState, useEffect } from "react";
 import { Dimensions, Pressable } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const { height, width } = Dimensions.get("screen") 
+const { height, width } = Dimensions.get("screen");
 
-export default function({selectedToilet}) {
+export default function ({ selectedToilet }) {
   const navigation = useNavigation();
-  const [toiletLikes, setToiletLikes] = useState(+selectedToilet[0].votes)
-  const [liked, setLiked] = useState(false)
-  const handleToiletView = () => {navigation.navigate('ToiletView', {selectedToilet, toiletLikes})}
-  
-  const handleLikeIncrease = async () => {
-    if(!liked) {
-      setToiletLikes((currentLikes) => [+currentLikes+1])
-      setLiked(true)
-        try {
-          const id = [`${selectedToilet[0].id}`, selectedToilet[0].id]
-          const count = [`${selectedToilet[0].id}count`, (+selectedToilet[0].votes+1).toString()]
-          await await AsyncStorage.multiSet([id, count]);
-        } catch (e) {
-          console.log(e)
-        }
-      }
-    else {
-      setToiletLikes((currentLikes) => [+currentLikes-1])
-      setLiked(false)
-      const keys = [`${selectedToilet[0].id}`, `${selectedToilet[0].id}count`]
-        try {
-          await AsyncStorage.multiRemove(keys)
+  const [toiletLikes, setToiletLikes] = useState(+selectedToilet[0].votes);
+  const [liked, setLiked] = useState(false);
+  const handleToiletView = () => {
+    navigation.navigate("ToiletView", { selectedToilet, toiletLikes });
+  };
 
-        } catch(e) {
-          console.log(e)
-        }
+  const handleLikeIncrease = async () => {
+    if (!liked) {
+      setToiletLikes((currentLikes) => [+currentLikes + 1]);
+      setLiked(true);
+      try {
+        const id = [`${selectedToilet[0].id}`, selectedToilet[0].id];
+        const count = [
+          `${selectedToilet[0].id}count`,
+          (+selectedToilet[0].votes + 1).toString(),
+        ];
+        await await AsyncStorage.multiSet([id, count]);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setToiletLikes((currentLikes) => [+currentLikes - 1]);
+      setLiked(false);
+      const keys = [`${selectedToilet[0].id}`, `${selectedToilet[0].id}count`];
+      try {
+        await AsyncStorage.multiRemove(keys);
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }
+  };
 
   const getData = async () => {
     try {
@@ -48,54 +51,85 @@ export default function({selectedToilet}) {
 
   const getCounts = async () => {
     try {
-      const data = await AsyncStorage.getItem(`${selectedToilet[0].id}count`)
+      const data = await AsyncStorage.getItem(`${selectedToilet[0].id}count`);
       return data;
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       const storedToiletId = await getData();
-      const storedLikeCount = await getCounts()
-      if(storedToiletId === selectedToilet[0].id) {
-        setLiked(true)
-        setToiletLikes(storedLikeCount)
-      }
-      else {
-        setLiked(false)
+      const storedLikeCount = await getCounts();
+      if (storedToiletId === selectedToilet[0].id) {
+        setLiked(true);
+        setToiletLikes(storedLikeCount);
+      } else {
+        setLiked(false);
       }
     };
     fetchData();
-  }, [selectedToilet]); 
-  
-  return(
-    <Block flex center row>
-      <Block flex={1} center>
-        {!liked ? 
-        <Pressable onPress={handleLikeIncrease}>
-          <Icon style={{textAlign:"center"}} name={"heart"} size={20} color="#E83E8C" family="feather"/>
-          <Text bold={true} color="#050505" center>{toiletLikes}</Text>
-        </Pressable>
-        :
-        <Pressable onPress={handleLikeIncrease}>
-          <Icon style={{textAlign:"center"}} name={"heart"} size={20} color="#E83E8C" family="antdesign"/>
-          <Text bold={true} color="#050505" center>{toiletLikes}</Text>
-        </Pressable>
-        }
+  }, [selectedToilet]);
+
+  return (
+    <>
+      <Block row safe>
+        <Block flex>
+          {!liked ? (
+            <Pressable onPress={handleLikeIncrease}>
+              <Icon
+                style={{ textAlign: "center" }}
+                name={"heart"}
+                size={20}
+                color="#E83E8C"
+                family="feather"
+              />
+              <Text bold={true} color="#050505" center>
+                {toiletLikes}
+              </Text>
+            </Pressable>
+          ) : (
+            <Pressable onPress={handleLikeIncrease}>
+              <Icon
+                style={{ textAlign: "center" }}
+                name={"heart"}
+                size={20}
+                color="#E83E8C"
+                family="antdesign"
+              />
+              <Text bold={true} color="#050505" center>
+                {toiletLikes}
+              </Text>
+            </Pressable>
+          )}
+        </Block>
+        <Block flex>
+          <Pressable center onPress={handleToiletView}>
+            <Icon
+              style={{ textAlign: "center" }}
+              name={"message-circle"}
+              size={20}
+              color="#E83E8C"
+              family="feather"
+            />
+            <Text bold={true} color="#050505" center>
+              {selectedToilet[0].comment_count}
+            </Text>
+          </Pressable>
+        </Block>
+        <Block flex>
+          <Pressable onPress={handleToiletView}>
+            <Icon
+              style={{ textAlign: "center" }}
+              name={"more-vertical"}
+              size={20}
+              color="#E83E8C"
+              family="feather"
+            />
+          </Pressable>
+        </Block>
       </Block>
-      <Block flex={1} center>
-        <Pressable center onPress={handleToiletView}>
-          <Icon style={{textAlign:"center"}} name={"message-circle"} size={20} color="#E83E8C" family="feather"/>
-          <Text bold={true} color="#050505" center>{selectedToilet[0].comment_count}</Text>
-        </Pressable>
-      </Block>
-      <Block flex={1} center>
-        <Pressable onPress={handleToiletView}>
-          <Icon style={{textAlign:"center"}} name={"more-vertical"} size={20} color="#E83E8C" family="feather"/>
-        </Pressable>
-      </Block>
-    </Block>)
+    </>
+  );
 }
