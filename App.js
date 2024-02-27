6e20952ec580a6e1265e55da9c7d875030ac09ef
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-27 11:00:44 +0000

    aligned the spinner in middle

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import NavTabs from './src/components/NavTabs';
-import {OriginLocationProvider} from './src/context/OriginLocation'
+import { StatusBar } from "expo-status-bar";
+import NavTabs from "./src/components/NavTabs";
+import { OriginLocationProvider } from "./src/context/OriginLocation";
 import { ToiletResponseProvider } from "./src/context/ToiletResponse";
 
+import * as SplashScreen from "expo-splash-screen";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { AppReadyContext, AppReadyContextProvider } from "./src/context/AppReady";
+
+SplashScreen.preventAutoHideAsync();
+
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(true);
+  
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       console.log("promiser countdown");
+  //       await new Promise((resolve) => setTimeout(resolve, 2000));
+  //       console.log(appIsReady, "<--appisReady state");
+  //       console.log(setAppIsReady, "<--setAppIsReady state");
+  //     } catch (e) {
+  //       console.warn(e);
+  //     } finally {
+  //       setAppIsReady(true);
+  //     }
+  //   }
+
+  //   prepare();
+  // }, [appIsReady]);
+
+  const onLayoutRootView = useCallback(async () => {
+    console.log(appIsReady,'app is ready value in app')
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <ToiletResponseProvider>
-      <OriginLocationProvider>
-        <NavTabs />
-      </OriginLocationProvider>
-    </ToiletResponseProvider>
+    <AppReadyContext.Provider value={{ appIsReady, setAppIsReady}}>
+      <ToiletResponseProvider>
+        <OriginLocationProvider>
+          <NavTabs />
+        </OriginLocationProvider>
+      </ToiletResponseProvider>
+    </AppReadyContext.Provider>
   );
 }
--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -5,102 +5,131 @@ import { Block } from "galio-framework";
 
 import { ToiletResponseContext } from "../context/ToiletResponse";
 import { OriginLocationContext } from "../context/OriginLocation";
+import { AppReadyContext } from "../context/AppReady";
 
 import FullMapView from "../components/results-screen/FullMapView";
 import NoToiletResult from "../components/results-screen/NoToiletResult";
-
 import { fetchCityToilets, fetchData } from "../api";
 import ListView from "../components/results-screen/ListView";
+import LoadingScreen from "../components/LoadingScreen";
+
 
 export default function ResultScreen() {
   const [fullMap, setFullMap] = useState(false);
-  const [selectedToilet, setSelectedToilet] = useState(null)
+  const [selectedToilet, setSelectedToilet] = useState(null);
   const [initialRegion, setInitialRegion] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [noToilets, setNoToilets] = useState(false);
 
+  const {appIsReady, setAppIsReady } = useContext(AppReadyContext)
   const { setToiletResponse } = useContext(ToiletResponseContext);
   const { originLocation } = useContext(OriginLocationContext);
+
+  async function prepareDataForGeoLocation(){
+    console.log('I am rendering geolocation')
+    setNoToilets(false);
+    setIsLoading(true);
+    fetchData(originLocation)
+    .then((response) => {
+      let nearbyToilets
+    try{  if(originLocation.changingTable) {
+          nearbyToilets = response.filter((toilet) => {
+          if(toilet.distance <= 10 && toilet.changing_table) {
+            return toilet
+          }
+        })
+      } else 
+        nearbyToilets = response.filter((toilet) => {
+        if (toilet.distance <= 10) {
+          return toilet;
+        }
+      });
+      if (nearbyToilets.length < 1) {
+        setNoToilets(true);
+      } else {
+        setToiletResponse(nearbyToilets);
+        setIsLoading(false);
+        setNoToilets(false);
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+
+    }
+  })}
+
+  async function prepareDataForDropDownCities(){
+    console.log('I am rendering dropdown')
+    try {
+      setAppIsReady(false)
+      
+      fetchCityToilets(originLocation)
+          .then((toiletData) => {
+            const cleanedData = toiletData.map((toilet) => {
+              toilet.id = toilet._id;
+              delete toilet._id;
+              return toilet;
+            });
+            if (cleanedData.length < 1) {
+              setNoToilets(true);
+            } else {
+              setToiletResponse(cleanedData);
+              setIsLoading(false);
+              setNoToilets(false);
+            }
+          })
+    } catch(err){
+          console.log(err);
+        } finally {
+          console.log(appIsReady,'hello')
+          setAppIsReady(true)
+        }
+  }
 
   useEffect(() => {
     setNoToilets(false);
     setIsLoading(true);
-    if(originLocation.city === undefined) {
-    !Object.keys(originLocation).length
-      ? null
-      : fetchData(originLocation)
-        .then((response) => {
-          let nearbyToilets
-          if(originLocation.changingTable) {
-              nearbyToilets = response.filter((toilet) => {
-              if(toilet.distance <= 10 && toilet.changing_table) {
-                return toilet
-              }
-            })
-          } else 
-            nearbyToilets = response.filter((toilet) => {
-            if(toilet.distance <= 10) {
-              return toilet
-            }
-          })
-          if (nearbyToilets.length < 1) {
-            setNoToilets(true);
-          } else {
-            setToiletResponse(nearbyToilets);
-            setIsLoading(false);
-            setNoToilets(false);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    else {
+    if (originLocation.city === undefined) {
       !Object.keys(originLocation).length
-      ? null
-      :
-      fetchCityToilets(originLocation)
-      .then((toiletData) => {
-        const cleanedData = toiletData.map((toilet) => {
-          toilet.id = toilet._id
-          delete toilet._id
-          return toilet
-        })
-        if (cleanedData.length < 1) {
-          setNoToilets(true);
-        } else {
-          setToiletResponse(cleanedData);
-          setIsLoading(false);
-          setNoToilets(false);
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+        ? null
+        : prepareDataForGeoLocation()
+    } else {
+      !Object.keys(originLocation).length
+        ? null : prepareDataForDropDownCities()
     }
   }, [originLocation]);
-  
+
   return (
     <>
       {noToilets ? (
         <>
-        <StatusBar />
-        <NoToiletResult />
+          <StatusBar />
+          <NoToiletResult />
         </>
       ) : (
         <>
-        <StatusBar />
-        <Block flex style={styles.container}>
-          {isLoading ? (
-            <Block>
-              <ActivityIndicator size="large" color="blue" />
-            </Block>
-          ) : fullMap ? (
-            <FullMapView setFullMap={setFullMap} initialRegion={initialRegion} setInitialRegion={setInitialRegion}/>
-          ) : (
-            <ListView setFullMap={setFullMap} initialRegion={initialRegion} setInitialRegion={setInitialRegion} setSelectedToilet={setSelectedToilet} selectedToilet={selectedToilet}/>
-          )}
-        </Block>
+          <StatusBar />
+          <Block flex style={styles.container}>
+            {isLoading ? (
+              <Block>
+                <ActivityIndicator size="large" color="blue" />
+              </Block>
+            ) : fullMap ? (
+              <FullMapView
+                setFullMap={setFullMap}
+                initialRegion={initialRegion}
+                setInitialRegion={setInitialRegion}
+              />
+            ) : (
+              <ListView
+                setFullMap={setFullMap}
+                initialRegion={initialRegion}
+                setInitialRegion={setInitialRegion}
+                setSelectedToilet={setSelectedToilet}
+                selectedToilet={selectedToilet}
+              />
+            )}
+          </Block>
         </>
       )}
     </>
